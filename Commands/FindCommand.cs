ca6bd91fc909cd19b5856b315a3dde4e89bd2e50
Walter Cain <30776780+walterhcain@users.noreply.github.com>
2026-03-12 10:23:22 -0700

    Add files via upload

---
+++ Commands/FindCommand.cs
@@ -0,0 +1,73 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using Rocket.Core.Logging;
+
+namespace walterhcain.CheckOwner
+{
+    public class Find : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+
+        public string Name
+        {
+            get { return "find"; }
+        }
+
+        public string Help
+        {
+            get { return "Finds coordinates of player"; }
+        }
+
+        public string Syntax
+        {
+            get { return "/find"; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>
+                {
+                  "cain.find"
+                };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (caller is UnturnedPlayer)
+            {
+                UnturnedPlayer player = (UnturnedPlayer)caller;
+                if (command.Length == 0)
+                {
+                    UnturnedChat.Say(caller, player.Position.x + " " + player.Position.y + " " + player.Position.z);
+                }
+                else if (command.Length == 1)
+                {
+                    UnturnedPlayer target = UnturnedPlayer.FromName(command[0]);
+                    UnturnedChat.Say(player, "The target is located at:");
+                    UnturnedChat.Say(player, target.Position.x + " " + target.Position.y + " " + target.Position.z);
+                }
+                else
+                {
+                    UnturnedChat.Say(player, "Improper Parameters", UnityEngine.Color.red);
+                }
+            }
+            else
+            {
+                Logger.Log("This command can only be called by a player.");
+            }
+
+        }
+    }
+}
---
+++ Commands/OwnerCheckerCommand.cs
@@ -0,0 +1,97 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace walterhcain.CheckOwner
+{
+    class OwnerCheckerCommand : IRocketCommand
+    {
+        RaycastHit hit;
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+
+        public string Name
+        {
+            get { return "checkowner"; }
+        }
+
+        public string Help
+        {
+            get { return "Check the owner of a certain object"; }
+        }
+
+        public string Syntax
+        {
+            get { return "/checkowner"; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (Physics.Raycast(player.Player.look.aim.position, player.Player.look.aim.forward, out hit, 10, RayMasks.BARRICADE_INTERACT))
+            {
+                byte x;
+                byte y;
+
+                ushort plant;
+                ushort index;
+
+                BarricadeRegion r;
+                StructureRegion s;
+
+                Transform transform = hit.transform;
+                InteractableVehicle vehicle = transform.gameObject.GetComponent<InteractableVehicle>();
+
+                if (transform.GetComponent<InteractableDoorHinge>() != null)
+                {
+                    transform = transform.parent.parent;
+                }
+                BarricadeDrop bd = BarricadeManager.FindBarricadeByRootTransform(transform);
+                StructureDrop sd = StructureManager.FindStructureByRootTransform(transform);
+                if(bd != null)
+                {
+                    BarricadeData bdata = bd.GetServersideData();
+                    Library.TellInfo(caller, (CSteamID)bdata.owner, (CSteamID)bdata.group);
+                }
+                else if (sd != null)
+                {
+                    StructureData sdata = sd.GetServersideData();
+
+                    Library.TellInfo(caller, (CSteamID)sdata.owner, (CSteamID)sdata.group);
+                }
+                else if (vehicle != null)
+                {
+                    if (vehicle.lockedOwner != CSteamID.Nil)
+                    {
+                        Library.TellInfo(caller, vehicle.lockedOwner, vehicle.lockedGroup);
+                        return;
+                    }
+                    UnturnedChat.Say(caller, "Vehicle does not have an owner.");
+                }
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>
+                {
+                  "cain.checkowner"
+                };
+            }
+        }
+    }
+}
+
